1ff4cfe9184dc152e6e302bbf5a87422f6b7a90a
dearaujofelipe <mgdefelipe@outlook.com>
2024-09-12 15:43:12 -0300

    mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB) // No need to pass any options
+  .then(() => console.log('DB connection success'))
+  .catch((err) => console.log('DB connection error: ', err));
 
 const app = require('./app');
 
