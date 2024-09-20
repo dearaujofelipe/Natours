b77951af5a419597993a66d0d3ccc07a84b340b7
dearaujofelipe <mgdefelipe@outlook.com>
2024-09-20 17:01:01 -0300

    error handlers

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('uncaughtException , SHUTTING IT DOWN...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -9,12 +15,16 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose
-  .connect(DB)
-  .then(() => console.log('DB connection success'))
-  .catch((err) => console.log('DB connection error: ', err));
+mongoose.connect(DB).then(() => console.log('DB connection success'));
+// .catch((err) => console.log('DB connection error: ', err));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('unhandledRejection , SHUTTING IT DOWN...');
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
 });
