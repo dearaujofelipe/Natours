0a8314ef0bfbac4ec68d1f233285873df89693a8
dearaujofelipe <mgdefelipe@outlook.com>
2024-09-22 14:23:39 -0300

    loggin users

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -18,12 +19,36 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'provide a password'],
     minLenght: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'confirme password'],
+    required: [true, 'confirm password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'password are not the same',
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
